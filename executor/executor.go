0895c0f6d6aba58858ede9ef8e8dd2ee3e019c36
Michael Collado <mrfritz379@michaels-mbp-3.lan>
2021-08-31 21:45:50 -0700

    Add executor main along with prometheus metrics collector

---
+++ executor/executor.go
@@ -0,0 +1,168 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
+	"github.com/PeladoCollado/imager/metrics"
+	"github.com/PeladoCollado/imager/orchestrator/logger"
+	"github.com/PeladoCollado/imager/orchestrator/manager"
+	"github.com/PeladoCollado/imager/types"
+	"github.com/PeladoCollado/imager/executor/worker"
+	"github.com/google/uuid"
+	"github.com/hashicorp/go-retryablehttp"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+	"time"
+)
+
+var orchestratorClient = newClient()
+
+func newClient() *http.Client {
+	client := retryablehttp.NewClient()
+	client.RetryMax = 5
+	client.RetryWaitMin = 100 * time.Millisecond
+	return client.StandardClient()
+}
+
+var workerId types.WorkerId
+
+func main() {
+	var orchestratorHost string
+	var port, workers int
+	flag.StringVar(&orchestratorHost, "host", "imgr-orchestrator",
+		"The hostname of the orchestrator process")
+	flag.IntVar(&port, "port", 8099, "The port of the orchestrator process")
+	flag.IntVar(&workers, "workers", 1, "The number of worker threads to start")
+	flag.Parse()
+	hostString := fmt.Sprintf("%s:%d", orchestratorHost, port)
+	connectUrl := fmt.Sprintf("http://%s/connect", hostString)
+
+	workerUuid, err :=  uuid.NewRandom()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to generate executor id %w", err)
+		os.Exit(1)
+	}
+	workerId = types.WorkerId{Id: workerUuid.String(), Workers: workers}
+	buffer := bytes.NewBuffer(make([]byte, 0, 1000))
+	jsonEncoder := json.NewEncoder(buffer)
+	err = jsonEncoder.Encode(workerId)
+	if err != nil {
+		logger.Logger.Error("Unable to encode worker as json", err)
+		os.Exit(1)
+	}
+	resp, err := orchestratorClient.Post(connectUrl, "application/javascript", buffer)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to orchestrator at %s:%p - %v\n", orchestratorHost, port, err)
+		os.Exit(1)
+	}
+	if resp.StatusCode != 200 {
+		errMsg, err := ioutil.ReadAll(io.LimitReader(resp.Body, 10000))
+		if err != nil {
+			errMsg = []byte(fmt.Sprintf("Unable to read error response body - %s", err.Error()))
+		}
+		fmt.Fprintf(os.Stderr, "Unable to connect to orchestrator at %s:%p status code %d- %s\n",
+			orchestratorHost, port, resp.StatusCode, string(errMsg))
+		os.Exit(1)
+	}
+	ctx, cancel := context.WithCancel(context.Background())
+
+	cancelChan := make(chan error)
+
+	// listen for cancellations
+	go func() {
+		<- cancelChan
+		cancel()
+	}()
+
+	go heartbeat(ctx, cancelChan, &http.Request{Method: "POST", URL: &url.URL{Host: hostString, Path: "/heartbeat"}})
+
+	collector := metrics.NewPrometheusMetricsCollector(prometheus.DefaultRegisterer)
+	http.Handle("/metrics", promhttp.Handler())
+
+	workChan := make(chan types.Job)
+	for i := 0; i < workers; i++ {
+		go runJob(ctx, workChan, collector)
+	}
+
+	poll(ctx, workers, &http.Request{Method: "GET", URL: &url.URL{Host: hostString, Path: "/next"}}, workChan, cancelChan)
+}
+
+func poll(ctx context.Context, workers int, req *http.Request, work chan types.Job, cancelChan chan error) {
+	request := req.WithContext(ctx)
+	buf := bytes.NewBuffer(make([]byte, 0, 1000))
+	encoder := json.NewEncoder(buf)
+	encoder.Encode(workerId)
+	for {
+		// reset the body on each request so the buffer starts from 0 each time
+		req.Body = ioutil.NopCloser(bytes.NewBuffer(buf.Bytes()))
+		resp, err := orchestratorClient.Do(request)
+		if err != nil {
+			cancelChan <- fmt.Errorf("unable to get job from orchestrator %w", err)
+			return
+		}
+		// orchestrator is telling us the job is done
+		if resp.StatusCode == 204 {
+			cancelChan <- errors.New("status complete")
+			return
+		}
+		if resp.StatusCode != 200 {
+			errorMsg, err := ioutil.ReadAll(io.LimitReader(resp.Body, 10000))
+			if err != nil {
+				errorMsg = []byte(fmt.Sprintf("Unable to read error message from orchestrator: %v", err))
+			}
+			cancelChan <- fmt.Errorf("error response fetching job from orchestrator: %d- %s",
+				resp.StatusCode,
+				string(errorMsg))
+			return
+		}
+		decoder := json.NewDecoder(resp.Body)
+		jobs := make([]types.Job, workers)
+		decoder.Decode(&jobs)
+		for j := range jobs {
+			work <- jobs[j]
+		}
+	}
+}
+
+func runJob(ctx context.Context, work chan types.Job, metricsCollector metrics.MetricsCollector) {
+	for {
+		select {
+		case job := <-work:
+			worker.RunJob(ctx, job, metricsCollector)
+		case <-ctx.Done():
+			return
+		}
+	}
+}
+
+var heartbeatError = errors.New("unable to publish heartbeat")
+
+func heartbeat(ctx context.Context, cancel chan error, req *http.Request) {
+	ticker := time.NewTicker(manager.HeartbeatFrequencySeconds * time.Second)
+	heartbeatFailures := 0
+	request := req.WithContext(ctx)
+	for {
+		select {
+		case <-ticker.C:
+			resp, err := orchestratorClient.Do(request)
+			if err != nil || resp.StatusCode != 200 {
+				heartbeatFailures++
+			}
+			if heartbeatFailures > manager.MaxMissedHeartbeats {
+				fmt.Fprintln(os.Stderr, "Unable to publish heartbeat to orchestrator. Canceling executor process")
+				cancel <- heartbeatError
+			}
+		case <-ctx.Done():
+			return
+		}
+	}
+}
--- metrics/metrics.go
+++ metrics/metrics.go
@@ -1,6 +1,7 @@
 package metrics
 
 import (
+	"github.com/prometheus/client_golang/prometheus"
 	"time"
 )
 
@@ -20,4 +21,92 @@ type ErrorEvent struct {
 type MetricsCollector interface {
 	PostSuccess(event SuccessEvent)
 	PostFailure(event ErrorEvent)
+}
+
+func NewPrometheusMetricsCollector(r prometheus.Registerer) MetricsCollector {
+	c := &PrometheusMetricsCollector{
+		duration: prometheus.NewHistogram(prometheus.HistogramOpts{Name: "duration",
+			Namespace: "imager",
+			Help:      "Request duration",
+			Buckets:   timeBuckets()}),
+		successDuration: prometheus.NewHistogram(prometheus.HistogramOpts{Name: "successDuration",
+			Namespace: "imager",
+			Help:      "Successful request duration",
+			Buckets:   timeBuckets()}),
+		responseSize: prometheus.NewHistogram(prometheus.HistogramOpts{Name: "responseSize",
+			Namespace: "imager",
+			Help:      "Response size",
+			Buckets:   sizeBuckets()}),
+		firstByteDuration: prometheus.NewHistogram(prometheus.HistogramOpts{Name: "firstByteDuration",
+			Namespace: "imager",
+			Help:      "Time to first byte",
+			Buckets:   timeBuckets()}),
+		successCounter: prometheus.NewCounter(prometheus.CounterOpts{Name: "success",
+			Namespace: "imager",
+			Help:      "Number of successful requests served"}),
+		failedCounter: prometheus.NewCounter(prometheus.CounterOpts{Name: "failed",
+			Namespace: "imager",
+			Help:      "Number of failed requests"}),
+	}
+	r.MustRegister(c.duration, c.successDuration, c.responseSize, c.firstByteDuration, c.successCounter, c.failedCounter)
+	return c
+}
+
+func timeBuckets() []float64 {
+	bucket := float64(10)
+	buckets := make([]float64, 0, 204)
+	for i := 0; i < 204; i++ {
+		buckets = append(buckets, bucket)
+		if bucket < 100 {
+			bucket += 5
+		} else if bucket < 1000 {
+			bucket += 25
+		} else if bucket < 10000 {
+			bucket += 100
+		} else if bucket < 60000 {
+			bucket += 1000
+		}
+	}
+	return buckets
+}
+
+const MB = 1 << 20
+
+func sizeBuckets() []float64 {
+	bucket := float64(64)
+	buckets := make([]float64, 0, 127)
+	for bucket < MB {
+		buckets = append(buckets, bucket)
+		bucket *= 2
+	}
+	for bucket < 10*MB {
+		buckets = append(buckets, bucket)
+		bucket += 1024 * 128
+	}
+	for bucket < 50*MB {
+		bucket += MB
+	}
+	return buckets
+}
+
+type PrometheusMetricsCollector struct {
+	duration          prometheus.Histogram
+	successDuration   prometheus.Histogram
+	responseSize      prometheus.Histogram
+	firstByteDuration prometheus.Histogram
+	successCounter    prometheus.Counter
+	failedCounter     prometheus.Counter
+}
+
+func (b *PrometheusMetricsCollector) PostSuccess(event SuccessEvent) {
+	b.duration.Observe(float64(event.Duration.Milliseconds()))
+	b.successDuration.Observe(float64(event.Duration.Milliseconds()))
+	b.responseSize.Observe(float64(event.ResponseSize))
+	b.firstByteDuration.Observe(float64(event.FirstByteTime.Milliseconds()))
+	b.successCounter.Inc()
+}
+
+func (b *PrometheusMetricsCollector) PostFailure(event ErrorEvent) {
+	b.duration.Observe(float64(event.Duration.Milliseconds()))
+	b.failedCounter.Inc()
 }
